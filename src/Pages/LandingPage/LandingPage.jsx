5278c722d4a1daa331996553f3ff81012c2bcd48
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-06-29 13:37:32 -0400

    set up react router

---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./LandingPage.scss"
+import Footer from '../../Components/Footer/Footer';
+
+
+function LandingPage(props) {
+    return (
+        <div>
+            <Footer />
+        </div>
+    );
+}
+
+export default LandingPage;
